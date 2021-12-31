c78618f1521f57db9f6703b6a34481745c6c5a25
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-01-01 01:55:13 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+```
+@@@  @@@  @@@ @@@@@@@@ @@@@@@@  @@@@@@@   @@@@@@  @@@  @@@
+ @@!  @@!  @@! @@!      @@!  @@@ @@!  @@@ @@!  @@@ @@!  @@@
+ @!!  !!@  @!@ @!!!:!   @!@!@!@  @!@  !@! @!@!@!@! @!@  !@!
+  !:  !!:  !!  !!:      !!:  !!! !!:  !!! !!:  !!!  !: .:!
+   ::.:  :::   : :: ::: :: : ::  :: :  :   :   : :    ::
+```
+
+
+## Installation
+
+• Installing via github 
+
+```console
+# clone the repo
+$ git clone https://github.com/
+
+# change the working directory to webserver
+$ cd webserver
+
+# install the requirements
+$ python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+<p align="center">
+<img src="./images/usage.gif"/>
+</p>
+
+This application is too user friendly there is even no costum argumants right now but I will add it as soon as possible.
+
+## Notes
+
+On windows , if you run this application on command prompt or powershell colors don't gonna work because of the input method they are using is different They don't color input prints so they look weird but here is the windows version .
+```
+```
---
+++ webserver.py
@@ -0,0 +1,61 @@
+import http.server
+import socketserver
+import functools
+from colorama import Fore, Back, Style, init, deinit
+
+init()
+
+banner = '''
+
+@@@  @@@  @@@ @@@@@@@@ @@@@@@@  @@@@@@@   @@@@@@  @@@  @@@
+ @@!  @@!  @@! @@!      @@!  @@@ @@!  @@@ @@!  @@@ @@!  @@@
+ @!!  !!@  @!@ @!!!:!   @!@!@!@  @!@  !@! @!@!@!@! @!@  !@!
+  !:  !!:  !!  !!:      !!:  !!! !!:  !!! !!:  !!!  !: .:!
+   ::.:  :::   : :: ::: :: : ::  :: :  :   :   : :    ::
+   
+'''
+
+usg = '''
+by aydeniz
+'''
+
+to_exit = '''
+     __________________       ________       
+    ||      ctrl      ||     ||  c   ||      
+    ||________________||  +  ||______||     
+    |/________________\|     |/______\|      
+'''
+
+about = '''
+author: Ahmet Yiğit AYDENİZ
+
+•github:https://github.com/Aydeniztr
+•Website:https://www.aydeniz.tk
+ 
+  • 🔭 I’m currently working on my own websit
+  • 🌱 I’m currently learning "c" and "sh"
+  • 👯 I’m looking to not collaborate on my projects
+  • 🤔 I’m looking for help with learning c++
+  • 💬 Ask me about linux,static-websites,python
+  • 📫 How to reach me : https://www.aydeniz.tk/chat.html
+'''
+
+print(Fore.RED + banner+ Fore.WHITE + usg + Fore.WHITE + about)
+
+port = int(input ( Fore.WHITE +'['+ Fore.GREEN + '*' + Fore.WHITE + ']' + Fore.YELLOW + 'select a port number ' + Fore.GREEN + ' (select between 1023-65536)\n'))
+
+if 1024 <= port <= 65535:
+
+  root = str(input('\n' + Fore.WHITE +'['+ Fore.GREEN + '*' + Fore.WHITE + ']' + Fore.YELLOW + 'select a root directory to run the server\n'+ Fore.GREEN ))
+
+  Handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory = root)
+
+  with socketserver.TCPServer(("", port), Handler) as httpd:
+  
+    print('\n'+Fore.WHITE +'['+ Fore.GREEN + '+' + Fore.WHITE + ']' + Fore.YELLOW + "Server started at localhost:" +Fore.YELLOW + str(port)+' ctrl+c to exit\n\n'+Fore.RED+ to_exit +'\n'+Fore.YELLOW)
+    httpd.serve_forever()
+      
+else:
+  print('\n'+ Fore.RED +'(!)'+' invalid port exiting ... pls select a valid port ')
+  deinit()
+  exit()
