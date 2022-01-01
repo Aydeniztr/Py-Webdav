a36bcc2a7636e7ccec7043930a190a611c5606f9
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-01-01 20:10:44 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -5,6 +5,9 @@ from colorama import Fore, Back, Style, init, deinit
 
 init()
 
+
+Roses are \textcolor{red}{red}, violets are \textcolor{blue}{blue}.
+
 banner = '''
 
 @@@  @@@  @@@ @@@@@@@@ @@@@@@@  @@@@@@@   @@@@@@  @@@  @@@
